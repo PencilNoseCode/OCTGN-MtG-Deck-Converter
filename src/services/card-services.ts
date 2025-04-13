e540887d7eff7c478821e00f17f36c580f9a4556
danielanapoli <dnapol01@gmail.com>
2025-04-13 15:15:43 -0400

    cleaned up code, TODO: wait for getCardID to return before pushing to deck

--- src/services/card-services.ts
+++ src/services/card-services.ts
@@ -1,5 +1,20 @@
-//import { ScriptElementKind } from 'typescript'
-//import { getCardID } from '../providers/scryfall-data-provider'
+/**
+ * ideal processing sequence:
+ *  1. parse content per line
+ *  2a. if content line element is a Zone then ...
+ *  2b. if content line element is not a Zone then treat it as a card
+ *      i. update card quantity, card name
+ *      ii. get card ID
+ *      iii. update card ID
+ *      iv. add initialized card to deck
+ *  3. after deck has been built, send deck
+ */
+
+import { getCardID } from '../providers/scryfall-data-provider';
+import UserCard from '../types/user-card';
+import { newCard } from '../types/user-card';
+//import Deck from "../types/deck";
+//import { newDeck } from "../types/deck";
 
 const ZONES: string[] = [
     'Main',
@@ -8,35 +23,51 @@ const ZONES: string[] = [
     'Planes/Schemes',
     'Magic the Gathering',
 ];
-//const DECK:string[] = [];
 
+var deck: Array<UserCard> = []; // consider not having an environmental variable
+
+/**
+ * @param content from uploaded text file
+ * @returns Array<UserCard>
+ *
+ * @todo figure out how to handle Zones
+ * @todo return custom error messages
+ */
 export function parseContent(content: string) {
     if (!content) {
-        console.log('content undefined');
+        return; // abort parsing
     }
-    let deck = content.split(/\r?\n/);
-    deck.forEach((element: string) => {
+    content.split(/\r?\n/).forEach((element: string) => {
         if (!element) {
-            console.log('element is undefined');
+            return; // skip element
         }
         if (ZONES.includes(element)) {
-            console.log('element is not a card'); // skip for now
+            return; // skip element, for now
         } else {
-            window.setTimeout(() => {
-                buildDeck(
-                    Number(element.substring(0, element.indexOf('x'))),
-                    String(element.split('x')[element.indexOf('x')]).trimStart()
-                );
-            }, 100);
+            createCard(
+                Number(element.substring(0, element.indexOf('x'))),
+                String(element.split('x')[element.indexOf('x')]).trimStart()
+            );
         }
     });
+    console.log('Content parsing completed. Deck returned.');
+    console.log(deck);
+    return deck;
 }
 
-function buildDeck(cardQuant: number, cardName: string) {
+/**
+ * @param cardQuant quantity of a specific card in the deck
+ * @param cardName name of a specific card in the deck
+ * @implements 100ms delay per card creation
+ * ! updates the environmental deck object
+ *
+ * @todo fix synchronousity issue where card is pushed before getCardID returns
+ */
+function createCard(cardQuant: number, cardName: string) {
     if (!cardQuant || !cardName) {
-        console.log('invalid buildDeck parameters');
+        console.log('invalid card parameters');
     }
-    console.log('Adding %d of card named "%s"', cardQuant, cardName);
-    //console.log('CardID: %s', getCardID(cardName))
-    //console.log(getCardID(cardName)); // takes in card name, returns card id
+    window.setTimeout(() => {
+        deck.push(newCard(cardQuant, cardName, String(getCardID(cardName)))); // TODO: decouple deck push and create card if needed
+    }, 100);
 }
