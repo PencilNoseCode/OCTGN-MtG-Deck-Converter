7be4fd43822502eb35e6a3dcf6d1bc976b8dfbc0
Tyler Jass <jasstsg@gmail.com>
2025-04-12 23:22:22 -0400

    added really basic UI, displaying the file name, and download the o8d xml with the same file name

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './App.css';
 import ContentUpload from './components/content-upload';
 import ContentDownload from './components/content-download';
 import { processCard } from './providers/scryfall-data-provider';
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
 
 function App() {
     const [content, setContent] = useState('');
+    const [selectedFile, setSelectedFile] = useState('No file selected');
 
     useEffect(() => {
         if (content) {
@@ -16,14 +19,23 @@ function App() {
     processCard('Blood Scrivener'); // takes in card name, returns card id
 
     return (
-        <div className="App">
-            <ContentUpload onUpload={setContent} />
-            <ContentDownload
-                content={content}
-                fileType="text/xml"
-                fileName="file.o8d"
-            />
-        </div>
+        <Container className="App">
+            <Stack gap={3}>
+                <h3>Select your exported MtG Deck text file from OCTGN</h3>
+                <ContentUpload
+                    label="Browse"
+                    selectedFile={selectedFile}
+                    setSelectedFile={setSelectedFile}
+                    onUpload={setContent}
+                />
+                <ContentDownload
+                    label="Download"
+                    content={content}
+                    fileType="text/xml"
+                    fileName={selectedFile.replace('.txt', '.o8d')}
+                />
+            </Stack>
+        </Container>
     );
 }
 
