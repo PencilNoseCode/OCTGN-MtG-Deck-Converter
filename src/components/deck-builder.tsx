2ccd6396228d39c2995f8e782f87607046764049
danielanapoli <dnapol01@gmail.com>
2025-04-26 23:05:00 -0400

    React Bootstracp for Deck View component

--- src/components/deck-builder.tsx
+++ src/components/deck-builder.tsx
@@ -1,5 +1,16 @@
+import { Anchor, Container } from 'react-bootstrap';
+import Tab from 'react-bootstrap/Tab';
+
 export function DeckBuilder() {
     // Some logic goes here
 
-    return <div>{/* react bootstrap components go here */}</div>;
+    return (
+        <Container className="DeckBuilder">
+            <Anchor className="deckbuilder"></Anchor>
+            <br />
+            <h1>Deck Builder</h1>
+            <Tab.Container></Tab.Container>
+            <br />
+        </Container>
+    );
 }
